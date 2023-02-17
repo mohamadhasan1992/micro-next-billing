0dcbc8857465b2c7fc35cc113568ed53dbb688d1
Mohamadhasan Tabrizi <tabrizi.mohamadhasan@yahoo.com>
2023-02-17 15:45:05 +0330

    connect apps

---
+++ apps/auth/src/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user.request';
+import { UsersService } from './users.service';
+
+@Controller('auth/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() request: CreateUserRequest) {
+    console.log(request)
+    return this.usersService.createUser(request);
+  }
+}
